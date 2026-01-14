ddfd64650a8bbfef415d06275a96848e4daf2420
Rassssven <dtimonea@gmail.com>
2026-01-14 18:54:04 +0200

    Initiere proiect

---
+++ proiect-kafka-redis/src/main/java/com/proiect_kafka_redis/proiect_kafka_redis/ProiectKafkaRedisApplication.java
@@ -0,0 +1,13 @@
+package com.proiect_kafka_redis.proiect_kafka_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectKafkaRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectKafkaRedisApplication.class, args);
+	}
+
+}
---
+++ proiect-kafka-redis/src/test/java/com/proiect_kafka_redis/proiect_kafka_redis/ProiectKafkaRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect_kafka_redis.proiect_kafka_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectKafkaRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
